7123e2f470ce2fab0f695c75632d1a35bd99b0a5
boolean5 <georgia_5@yahoo.com>
2015-04-01 06:00:48 +0200

    merged AIDrive with AIStated, updated AIState, created AIDemo

---
+++ src/AIDemo.java
@@ -0,0 +1,75 @@
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Properties;
+
+import java.lang.System;
+
+import aima.search.framework.Problem;
+import aima.search.framework.Search;
+import aima.search.framework.SearchAgent;
+import aima.search.informed.HillClimbingSearch;
+import aima.search.informed.SimulatedAnnealingSearch;
+
+
+public class AIDemo {
+    
+    public static void main(String[] args){
+    	// What value to use as a seed??
+        AIState State = new AIState(10,4,1);
+        AIHillClimbingSearch(State);
+        AISimulatedAnnealingSearch(State);
+    }
+    
+    private static void AIHillClimbingSearch(AIState State) {
+        System.out.println("\nAI HillClimbing  -->");
+        try {
+            Problem problem =  new Problem(State,new AISuccessorFunction(), new AIGoalTest(),new AIHeuristicFunction());
+            Search search =  new HillClimbingSearch();
+            SearchAgent agent = new SearchAgent(problem,search);
+            
+            System.out.println();
+            printActions(agent.getActions());
+            printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void AISimulatedAnnealingSearch(AIState State) {
+        System.out.println("\nAI Simulated Annealing  -->");
+        try {
+            Problem problem =  new Problem(State,new AISuccessorFunctionSA(), new AIGoalTest(),new AIHeuristicFunction());
+            //Check Simulated Annealing Parameters 
+            SimulatedAnnealingSearch search =  new SimulatedAnnealingSearch(2000,100,5,0.001);
+            //search.traceOn();
+            SearchAgent agent = new SearchAgent(problem,search);
+            
+            System.out.println();
+            printActions(agent.getActions());
+            printInstrumentation(agent.getInstrumentation());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    
+    private static void printInstrumentation(Properties properties) {
+        Iterator keys = properties.keySet().iterator();
+        while (keys.hasNext()) {
+            String key = (String) keys.next();
+            String property = properties.getProperty(key);
+            System.out.println(key + " : " + property);
+        }
+        
+    }
+    
+    private static void printActions(List actions) {
+        for (int i = 0; i < actions.size(); i++) {
+            String action = (String) actions.get(i);
+            System.out.println(action);
+        }
+    }
+    
+    
+}
+
--- src/AIState.java
+++ src/AIState.java
@@ -1,8 +1,50 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Random;
+
+import IA.Comparticion.Usuarios;
+import IA.Comparticion.Usuario;
 
 public class AIState {
-	List<Drive> drives;
+	
+	public static int numberOfUsers;
+	public static int numberOfDrivers;
+	
+	public static String MERGE = "Merge";
+	public static String MOVE = "Move";
+	public static String SWAP = "Swap";
+			
+	public class Drive {
+		int driverId;
+		int distance;
+		List<Integer> actions;
+	}
+	
+	ArrayList<Drive> drives;
+	ArrayList<Usuario> Users;
 	
 	int totalKilometers;
-	int totalDrivers;
+	int totalActiveDrivers;
+	
+	/* Constructor
+	 * Generates a new instance of the problem with N users
+	 * and M drivers
+	 */
+	public AIState(int N, int M, int seed){
+		
+		numberOfUsers = N;
+		numberOfDrivers = M;
+		
+		Users = new Usuarios(N, M, seed);	
+		
+		/* Generation of an initial solution of type 1
+		 * Assign passengers to the 1st driver until we reach the 30km limit
+		 * then move on to the 2nd one, etc.
+		 */
+		
+		
+	}
+	
+	
+	
 }
